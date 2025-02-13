0c2906b218b626586caab74c1d1fa3f43287a94a
Samiran2004 <samirans170@gmail.com>
2025-02-13 07:34:51 +0530

    add/variable.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+name = "Samiran"
+age = 20
+height = 5.8
+is_student = True
+
+x = 10
+y = 5
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(x+y)
+print(x>y)
+print(x and y > 0)
+x += 5
+print(x)
