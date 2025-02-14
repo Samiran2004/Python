c956e8538afd2ccd7fcb5f6dcef4bfd6bea8363c
Samiran2004 <samirans170@gmail.com>
2025-02-14 23:00:18 +0530

    add/sys module

---
+++ sysModule.py
@@ -0,0 +1,16 @@
+import sys
+
+print(sys.path)
+
+"""
+        Function	Description
+        sys.argv	Command-line arguments
+        sys.exit()	Exit the program
+        sys.version	Python version
+        sys.path	Module search paths
+        sys.stdin.read()	Read from standard input
+        sys.stdout.write()	Write to standard output
+        sys.stderr.write()	Write to error output
+        sys.maxsize	Max integer size
+        sys.platform	System platform name
+"""
