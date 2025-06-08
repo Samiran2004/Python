3fe09c0f29f855f7f0ac0a5e9ae7e4f1539ba53a
Samiran2004 <samirans170@gmail.com>
2025-06-08 11:13:55 +0530

    update/Day02

--- 30_Days_Python_DevOps/Day02.py
+++ 30_Days_Python_DevOps/Day02.py
@@ -5,3 +5,17 @@ print(arn.split("/")[1]) # Extract the username from arn string
 username = arn.split("/")[1]
 print(username.upper()) # Print the username in uppercase
 print(arn.split("/")[1].upper()) # This also works to print the username in uppercase in one line
+
+str1 = "Hello"
+str2 = "World!"
+print(str1+" "+str2) # Concat two strings
+
+import re
+str = "A quick brown fox."
+pattern = r"quick"
+match = re.match(pattern, str)
+
+if match:
+    print("Match found: ", match.group())
+else:
+    print("No match found.")
