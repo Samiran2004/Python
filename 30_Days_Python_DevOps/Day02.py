3255ba84772a3c5e5165cd77d4376d8c130dcd5e
Samiran2004 <samirans170@gmail.com>
2025-06-08 11:49:17 +0530

    update/Day02

--- 30_Days_Python_DevOps/Day02.py
+++ 30_Days_Python_DevOps/Day02.py
@@ -14,8 +14,40 @@ import re
 str = "A quick brown fox."
 pattern = r"quick"
 match = re.match(pattern, str)
+search = re.search(pattern, str)
+
+if search:
+    print("Search found: ", search.group())
+else:
+    print("Search Element Not Found.")
 
 if match:
     print("Match found: ", match.group())
 else:
     print("No match found.")
+
+text = "The quick brown fox jumps over the lazy brown dog"
+pattern = r"brown"
+replace_str = "red"
+new_txt = re.sub(pattern, replace_str, text)
+print("Modified text: ", new_txt)
+
+text = "apple,banana,orange,grape"
+pattern = r","
+split_result = re.split(pattern, text)
+print("Split result: ",split_result)
+
+print(len(username)) # Print the length of the username
+# print(len(arn.split("/")[0]))
+
+mod_arn = arn.replace(username, "samiran") # Modify the username
+print(mod_arn)
+
+text = "   Some spaces around   "
+stripped_txt = text.strip() # String strip
+print("Stripped text: ", stripped_txt)
+
+text = "Python is awesome"
+substring = "is"
+if substring in text:  # Found substring
+    print(substring, "found in the text")
