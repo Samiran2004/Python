6d481949c259d0ce5e2b3bdb9fa313c9a411ace5
Samiran2004 <samirans170@gmail.com>
2025-06-10 23:09:55 +0530

    update/folders, add/Day04

---
+++ 30_Days_Python_DevOps/Day04/calculator.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 5
+
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+print("Addition: " + str(add(num1, num2)))
+print("Substraction: "+ str(sub(num1, num2)))
+print("Multiplication: "+ str(mul(num1, num2)))
+print("Division: "+ str(div(num1, num2)))
