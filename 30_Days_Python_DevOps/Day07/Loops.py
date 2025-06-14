bcdf4b029723cb1065db41fc575e41a401e22bc3
Samiran2004 <samirans170@gmail.com>
2025-06-14 12:52:36 +0530

    add/Day07

---
+++ 30_Days_Python_DevOps/Day07/Loops.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(10):
+    print(i)
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
