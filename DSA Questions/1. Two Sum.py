3d928c44ab1b01baf68b165b6d7023f7015dd6fb
Samiran2004 <samirans170@gmail.com>
2025-10-18 08:38:37 +0530

    add/1. Two Sum

---
+++ DSA Questions/1. Two Sum.py
@@ -0,0 +1,30 @@
+from typing import List
+def twoSumBruteForce(nums: List[int], target: int)-> List[int]:
+    ans = []
+    for i in range(0, len(nums)-1):
+        for j in range(i, len(nums)):
+            if nums[i] + nums[j] == target:
+                ans.append(i)
+                ans.append(j)
+                return ans
+    return ans
+
+def twoSum(nums: List[int], target: int)-> List[int]:
+    prevMap = {}
+
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return []
+
+arr = list()
+size = int(input("Enter the size of the array: "))
+for _ in range(size):
+    data = int(input("Enter data: "))
+    arr.append(data)
+
+key = int(input("Enter the key data: "))
+print(twoSumBruteForce(nums=arr, target=key))
+print(twoSum(nums=arr, target=key))
