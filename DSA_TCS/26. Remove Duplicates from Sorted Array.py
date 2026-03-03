e3e62c0c320c573147d56976afd62a8ec29e55d0
Samiran2004 <samiran.samanta.dev@gmail.com>
2026-03-03 19:55:02 +0530

    update/26. Remove Duplicates from Sorted Array

--- DSA_TCS/26. Remove Duplicates from Sorted Array.py
+++ DSA_TCS/26. Remove Duplicates from Sorted Array.py
@@ -1,18 +1,15 @@
 def removeDuplicates(nums):
-    if len(nums) <= 0:
+    if len(nums) == 0:
         return 0
-    dict_map = {}
-    j = 0
-    
-    for i in nums:
-        dict_map[i] = 0
-    
-    for k in dict_map:
-        nums[j] = k
-        j += 1
-    
-    return j
-        
+
+    write_index = 1
+    for read_index in range(1, len(nums)):
+        if nums[read_index] != nums[write_index - 1]:
+            nums[write_index] = nums[read_index]
+            write_index += 1
+
+    return write_index
+
 nums = []
 size = int(input("Enter the size of the array: "))
 for _ in range(0, size):
